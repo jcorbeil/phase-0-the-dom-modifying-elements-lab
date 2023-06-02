b66c7718d0d372b383fb830cfd885f833df16a9f
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-02 12:57:43 -0400

    finished-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+/*document.addEventListener("DOMContentLoaded", function() {
+    const newHeader = document.createElement("h1");
+    newHeader.setAttribute("id", "victory");
+    const yourName = "Jeremy"; // Replace with your name
+    newHeader.textContent = `${yourName} is the champion`;
+    document.body.appendChild(newHeader);
+  });*/
+
+  const main = document.getElementById("main");
+  main.remove();
+  const newHeader = document.createElement("h1");
+  newHeader.setAttribute("id", "victory");
+  const yourName = "Jeremy"; // Replace with your name
+  newHeader.textContent = `${yourName} is the champion`;
+  document.body.appendChild(newHeader);
